cb205697fc00a92e72e97ca0edcbddfc91b38805
Jacob Cahoon <booka66@Jacobs-MacBook-Pro-2.local>
2025-10-05 11:38:51 -0600

    markdown preview :)

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,27 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = "cd app && npm install",
+  keys = {
+    { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
+  },
+  config = function()
+    vim.g.mkdp_auto_start = 0
+    vim.g.mkdp_auto_close = 1
+    vim.g.mkdp_refresh_slow = 0
+    vim.g.mkdp_command_for_global = 0
+    vim.g.mkdp_open_to_the_world = 0
+    vim.g.mkdp_browser = ""
+    vim.g.mkdp_echo_preview_url = 0
+    vim.g.mkdp_preview_options = {
+      mkit = {},
+      katex = {},
+      uml = {},
+      maid = {},
+      disable_sync_scroll = 0,
+      sync_scroll_type = "middle",
+      hide_yaml_meta = 1,
+    }
+  end,
+}
